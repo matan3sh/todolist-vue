5f295310d1e053557d89671671abbaa6af5e1def
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-06-16 10:06:38 +0300

    Initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input
+        type="text"
+        v-model="title"
+        name="title"
+        placeholder="Add Todo..."
+      />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+// import { uuid } from 'vue-uuid';
+export default {
+  name: 'AddTodo',
+  data() {
+    return {
+      title: ''
+    };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+        // id: uuid.v4(),
+        title: this.title,
+        completed: false
+      };
+      //   Send up to perent
+      this.$emit('add-todo', newTodo);
+      this.title = '';
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type='text'] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type='submit'] {
+  flex: 2;
+}
+</style>
